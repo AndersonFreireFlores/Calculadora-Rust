b902b471ca3de9f08c4fa957a5ce584a0acacfd4
ander <andersonfflores75@gmail.com>
2024-10-24 17:39:17 -0300

    versao com problema no match

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,54 @@
-fn main() {
+use std::io;
 
+fn main() {
+    loop {
+        println!("Calculadora");
+        println!("Insira um digito:");
+
+        let mut  x: String = String::new();
+
+        io::stdin().read_line(&mut x)
+            .expect("Failed to read line");
+
+        println!("Insira uma operacão:");
+
+        let mut  op: String = String::new();
+
+        io::stdin().read_line(&mut op)
+            .expect("Failed to read line");
+
+        println!("Insira outro digito:");
+
+        let mut  y: String = String::new();
+
+        io::stdin().read_line(&mut y)
+            .expect("Failed to read line");
+
+        let x: u32 = match x.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        let y: u32 = match y.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        let operacao = op.as_str();
+
+        match operacao {
+            "+" => println!("{}", x+y),
+            "-" => println!("{}", x-y),
+            "*" => println!("{}", x*y),
+            "x" => println!("{}", x*y),
+            "X" => println!("{}", x*y),
+            "/" => println!("{}", x/y),
+            "%" => println!("{}", x%y),
+            _ => {
+                println!("Não é uma operacão valida!");
+                continue
+            },
+        }
+        break
+    }
 }
